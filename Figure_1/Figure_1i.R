297f75270752d2971ad251c491558cdfe5444db7
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-29 15:36:16 -0400

    Update Figure_1i.R

--- Figure_1/Figure_1i.R
+++ Figure_1/Figure_1i.R
@@ -2,8 +2,11 @@ library(dplyr)
 library(ggplot2)
   
 
-
+# load sample informatiom
 pheno <- read.csv('../LS95_sample_info_03262024.csv')
 head(pheno)
 
 ordered_names <- unique(pheno$Names)
+
+# load metadata
+
