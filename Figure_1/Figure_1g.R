8274518a563979779aba37646a797d6b754baa9b
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-29 15:16:13 -0400

    Update Figure_1g.R

--- Figure_1/Figure_1g.R
+++ Figure_1/Figure_1g.R
@@ -1,5 +1,5 @@
-
-
+library(dplyr)
+library(ggplot2)
 
 # colors 
   cols_L2 <-  c('B_naive' = '#41b8ea',
@@ -23,3 +23,49 @@
 
 # load meta data 
 LifeSpan_ALL_MetaData <- read.csv("meta/LifeSpan_ALL_Annotated_MetaData_09122024.csv", row.names = 1) %>% as.data.frame()
+
+
+# level 2 (n=18 clusters)
+  order_pbmc_simple_clustering <- c('CD14_mo', 'CD16_mo', 'DCs', 'pDC', 'Mgk','HSPC',
+                                    'NK_CD16', 'NK_XCL1', 'gd_Tcells',
+                                    'B_naive', 'B_memory','PCs', 
+                                    'CD4_T_Naive','CD4_T_ISGhi', 'CD4_T_Memory', 'CD4_Tregs', 
+                                    'CD8_T_Naive', 'CD8_T_Effector')
+    
+# age groups 
+ age_groups <- c("HI", "HC", "HY", "HO")
+
+  my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
+  
+  # Subsets to plot 
+  subset_to_plot <- c("CD14_mo",'CD8_T_Effector',"CD4_T_Memory",'NK_CD16',"CD4_T_Naive",'CD8_T_Naive','B_naive','CD4_T_ISGhi')
+
+  plt_age1 <- LifeSpan_ALL_MetaData %>% 
+    
+    mutate(ReCluster = factor(pbmc_simple_clustering, levels = order_pbmc_simple_clustering)) %>%
+    mutate(Groups = factor(Groups, levels = age_groups)) %>%
+    group_by(Groups, Names, ReCluster) %>%
+    summarise(n = n()) %>% #, Set = first(Set)
+    mutate(freq = n / sum(n) *100) %>%
+    ungroup() %>%
+    as.data.frame() %>%
+    filter(ReCluster %in% subset_to_plot) %>% 
+    mutate(ReCluster = factor(ReCluster, levels = subset_to_plot)) %>%
+    ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = Groups)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter(size = 0.2) +
+    theme_bw()  +  #THEME +
+    #ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") +
+    ggpubr::stat_compare_means(comparisons = my_comparisons,  label = "p.signif", hide.ns = F, vjust = 0.5) + 
+    theme(legend.position = "none", 
+          strip.text = element_text(size = 13)) +
+    facet_wrap(.~ReCluster, scales = "free_y", nrow = 2) + 
+    
+    scale_fill_manual(values=cols) + #**
+    theme(axis.text.y=element_text(size=16), 
+          axis.text.x=element_text(size=16, angle =90),
+          axis.title.x = element_text(face="bold", size=18),
+          axis.title.y = element_text(face="bold", size=18)) + #    ylab('% PBMC') + xlab('Age groups')
+    ylab('% PBMC') + xlab('Age groups')
+  
+  print(plt_age1)
