547ca9a40afb9e983b6d04364880dfab2f17cfc5
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 11:45:30 -0400

    Update Figure_3c.R

--- Figure_3/Figure_3c.R
+++ Figure_3/Figure_3c.R
@@ -1,1 +1,43 @@
+library(dplyr)
+library(ggplot2)
+
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+cols <- c('NK_CD16'= '#fee000',
+            'NK_XCL1'= '#f2e4a0',
+            'NK_cycling'= '#ccb72d',
+            'NK_CD16_KLRC2'='#feb24c')
+
+# subset to be plotted 
+subset_to_be_plotted <-  c('NK_CD16', 'NK_XCL1','NK_CD16_KLRC2', 'NK_cycling')
+
+BP_gp <- LifeSpan_ALL_MetaData %>% 
+  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
+  mutate(ReCluster = factor(Final_annotations)) %>%
+  group_by(Groups, ReCluster) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
+  
+  summarise(n = n()) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>% #head()
+  
+  ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = ReCluster)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_manual(values=cols) + #***
+  scale_x_discrete(limits=c("HI", "HC", "HY", "HO")) + #labels= labels
+  theme(axis.text.y=element_text(size=18), 
+        axis.text.x=element_text(size=18, angle = 0),
+        axis.title.x = element_text(size=0),
+        axis.ticks.x=element_blank(),
+        axis.title.y = element_text(size=0),
+        legend.position = "none",
+        plot.title = element_text(face='bold', color = 'black', size = 20, hjust = 0.5)) +
+  ggtitle('Age groups')
+
+BP_gp
 
