d9e96d643780d1175d807d7020f25c5d35b6c55b
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:55:55 -0400

    Update Figure_5b.R

--- Figure_5/Figure_5b.R
+++ Figure_5/Figure_5b.R
@@ -1,6 +1,11 @@
 library(dplyr)
 library(ggplot2)
 
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+
 #color 
 cols <- c("CD4_T_Naive"="#193a1c",
             "CD4_T_ISGhi"="#697d35",
