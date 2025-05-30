24b44002748b23b826713570c3c64499a35fd925
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 11:57:32 -0400

    Update Figure_4d.R

--- Figure_4/Figure_4d.R
+++ Figure_4/Figure_4d.R
@@ -14,3 +14,45 @@ cols <- c('B_ISGhi'='#9ecae1',
             'B_transitional'='#756bb1',
             'B_naive'='#1c9099')
             
+
+
+# subset to be plotted 
+subset_to_be_plotted <-  c("B_naive","B_transitional","B_memory","B_ABC","B_ISGhi","PCs")
+
+
+plt_cor1 <- LifeSpan_ALL_MetaData %>%
+  
+  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
+  mutate(ReCluster = factor(Final_annotations, levels = ordered_SC)) %>% #*****
+  mutate(Age_days = Age_months*30) %>% 
+  group_by(Groups, Names,Age_months,Age_days, ReCluster) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
+  summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>%
+  
+  # subsets to be plotted   
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
+  # infants only 
+  filter(Groups %in% c('HI')) %>% 
+  
+  ggplot(aes(x = Age_months, y = freq, fill=ReCluster)) +
+  geom_smooth(method = "lm", aes(color=ReCluster)) + #, color = c('#f37421ff','#ffdeadff')
+  geom_point(aes(shape = Groups, color=ReCluster)) +
+  scale_fill_manual(values=cols) + 
+  scale_color_manual(values = cols)+
+  ggpubr::stat_cor() +
+  theme_bw() +
+  theme(legend.position = "none", 
+        strip.text = element_text(size = 14)) +
+  facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) +
+  theme(axis.text.y=element_text(size=12, colour = 'black'), 
+        axis.text.x=element_text(size=12, colour = 'black'),
+        axis.title.x = element_text(face="bold", size=14, colour = 'black'),
+        axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
+        strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) +#    ylab('% PBMC') + xlab('Age groups') #    ylab('% PBMC') + xlab('Age groups'
+  ylab('% of B cells') + xlab('Age (months)')
+
+print(plt_cor1)
+
