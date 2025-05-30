7c8bf8d9a137162b833ebccbe4d1beb6de65c6f5
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:31:45 -0400

    Update Figure_3d.R

--- Figure_3/Figure_3d.R
+++ Figure_3/Figure_3d.R
@@ -1,1 +1,13 @@
+library(dplyr)
+library(ggplot2)
 
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+cols <- c('NK_CD16'= '#fee000',
+            'NK_XCL1'= '#f2e4a0',
+            'NK_cycling'= '#ccb72d',
+            'NK_CD16_KLRC2'='#feb24c')
