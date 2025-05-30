73092e81ccb0312880a11d7b42f927bb72599f9c
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:27:16 -0400

    Update Figure_2e.R

--- Figure_2/Figure_2e.R
+++ Figure_2/Figure_2e.R
@@ -1,1 +1,13 @@
+library(dplyr)
+library(ggplot2)
+
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+cols <- c('CD14_mo_ISGhi'= '#f15d64',
+          'CD14_mo'= '#f6a2a7',
+          'CD16_mo'= '#f9d3d7')
 
