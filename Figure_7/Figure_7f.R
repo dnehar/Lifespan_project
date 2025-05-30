4587622d3022ce69253a0cc3f72a7f34bcbbd31e
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 12:06:48 -0400

    Update Figure_7f.R

--- Figure_7/Figure_7f.R
+++ Figure_7/Figure_7f.R
@@ -19,12 +19,12 @@ plt_cor1 <- LifeSpan_ALL_MetaData %>%
   mutate(ReCluster = factor(Final_annotations, levels = ordered_SC)) %>% #*****
   mutate(Age_days = Age_months*30) %>% 
   group_by(Groups, Names,Age_months,Age_days, ReCluster) %>%
-  filter(ReCluster %in% subset_to_be_plotted) %>% 
   summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
   mutate(freq = n / sum(n) *100) %>%
   ungroup() %>%
   as.data.frame() %>%
-  
+
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
 
   # infants only 
   filter(Groups %in% c('HI')) %>% 
@@ -38,12 +38,13 @@ plt_cor1 <- LifeSpan_ALL_MetaData %>%
   theme_bw() +
   theme(legend.position = "none", 
         strip.text = element_text(size = 14)) +
-  facet_wrap(.~ReCluster, scales = "free_y", nrow =2) +
+  facet_wrap(.~ReCluster, scales = "free_y", nrow =1) +
   theme(axis.text.y=element_text(size=12, colour = 'black'), 
         axis.text.x=element_text(size=12, colour = 'black'),
         axis.title.x = element_text(face="bold", size=14, colour = 'black'),
         axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
         strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) +#    ylab('% PBMC') + xlab('Age groups') #    ylab('% PBMC') + xlab('Age groups'
-  ylab('% of PBMCs') + xlab('Age (months)')
+  ylab('% PBMCs') + xlab('Age (months)')
 
 print(plt_cor1)
+
