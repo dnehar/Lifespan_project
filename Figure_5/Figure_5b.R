e6ef8dd0d6bb51df8f5debb2be2154c3c65c7bd1
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:43:09 -0400

    Update Figure_5b.R

--- Figure_5/Figure_5b.R
+++ Figure_5/Figure_5b.R
@@ -5,32 +5,16 @@ library(ggplot2)
 cols <- c("CD4_T_Naive"="#193a1c",
             "CD4_T_ISGhi"="#697d35",
             "CD4_Tregs"="#137d82",
-            "CD4_T_Memory"="#1c572b",
-            'CD4_ISGhi'= '#697d35',
-            'CD4_T_ISGhi'='#697d35',
-            #CD4_CTL'= '#90aa3c',
-            'CD4_Naive'= '#193a1c')
+            "CD4_T_Memory"="#1c572b")
             
-
-# plot umap CD4 T cells
-
-p_umap_subset <- LifeSpan_ALL_MetaData %>% 
-dplyr::filter(Lineage %in% c('CD4_Tcells')) %>% 
-mutate(subset_simple_clustering = gsub(pattern = "CD4_CTL", replacement = "CD4_T_Memory", x = subset_simple_clustering)) %>%
-ggplot(aes(x=SC_umap1, y=SC_umap2,  color=subset_simple_clustering)) +
-geom_point(size=0.05) + #, alpha = 1
-scale_color_manual(values=cols) + 
-theme_void() 
-print(p_umap_subset)
 
 age_groups <- c("HI", "HC", "HY", "HO")
 my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
 
 # subset to be plotted 
-subset_to_be_plotted <- c("B_naive","B_transitional","B_memory","B_ABC","B_ISGhi","PCs")
+subset_to_be_plotted <- c("CD4_T_Memory","CD4_Tregs","CD4_T_Naive","CD4_T_ISGhi" ) #"CD4_CTL",
 
 # plot box plot - age groups 
-
   plt_age <- LifeSpan_ALL_MetaData %>% 
             mutate(ReCluster = factor(subset_simple_clustering)) %>% #, levels = ordered_SC
             mutate(subset_simple_clustering = gsub(pattern = "CD4_CTL", replacement = "CD4_T_Memory", x = subset_simple_clustering)) %>%
@@ -46,9 +30,7 @@ subset_to_be_plotted <- c("B_naive","B_transitional","B_memory","B_ABC","B_ISGhi
     geom_boxplot(outlier.shape = NA) +
     geom_jitter(size = 0.2) +
     theme_bw()  +  #THEME +
-    #ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") +
     ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") + #label = "p.signif"
-    #ggpubr::stat_compare_means(comparisons = my_comparisons, label = "p.signif", hide.ns = F, vjust = 0.5) + 
     theme(legend.position = "none", 
           strip.text = element_text(size = 14, face='bold')) +
     facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) + 
