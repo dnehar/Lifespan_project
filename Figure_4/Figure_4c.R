1bf25a5b4b7ed7af795ba9694fca994f7c48b4a2
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 11:47:29 -0400

    Update Figure_4c.R

--- Figure_4/Figure_4c.R
+++ Figure_4/Figure_4c.R
@@ -14,15 +14,6 @@ cols <- c('B_ISGhi'='#9ecae1',
             'B_transitional'='#756bb1',
             'B_naive'='#1c9099')
             
-library(dplyr)
-library(ggplot2)
-
-
-# load metadata
-MetaData <- readRDS('./pbmcs_v1.rds')
-LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
-
-
 # subset to be plotted 
 subset_to_be_plotted <-  c('B_naive', 'B_transitional','B_ABC', 'B_memory','B_ISGhi','PCs')
 
