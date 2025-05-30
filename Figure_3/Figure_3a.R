1da05d6d22af27a26a2faf95ca162daad7d43525
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:02:22 -0400

    Update Figure_3a.R

--- Figure_3/Figure_3a.R
+++ Figure_3/Figure_3a.R
@@ -1,1 +1,23 @@
+library(dplyr)
+library(ggplot2)
 
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+cols <- c('NK_CD16'= '#fee000',
+            'NK_XCL1'= '#f2e4a0',
+            'NK_cycling'= '#ccb72d',
+            'NK_CD16_KLRC2'='#feb24c')
+            
+
+# plot umap DCs
+p_umap_subset <- meta_all %>% 
+dplyr::filter(Lineage %in% 'NK_cells') %>% 
+ggplot(aes(x=SC_umap1, y=SC_umap2,  color=Final_annotations)) +
+geom_point(size=0.5) + #, alpha = 1
+scale_color_manual(values=cols) + 
+theme_void() 
+print(p_umap_subset)
