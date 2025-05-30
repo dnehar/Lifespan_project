c670035e61baa483586612f098172b506033194a
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:28:07 -0400

    Update Figure_2e.R

--- Figure_2/Figure_2e.R
+++ Figure_2/Figure_2e.R
@@ -11,3 +11,13 @@ cols <- c('CD14_mo_ISGhi'= '#f15d64',
           'CD14_mo'= '#f6a2a7',
           'CD16_mo'= '#f9d3d7')
 
+
+# plot umap monocytes
+
+p_umap_subset <- meta_all %>% 
+dplyr::filter(Lineage %in% 'Monocytes') %>% 
+ggplot(aes(x=SC_umap1, y=SC_umap2,  color=Final_annotations)) +
+geom_point(size=0.2) + #, alpha = 1
+scale_color_manual(values=cols) + 
+theme_void() 
+print(p_umap_subset)
