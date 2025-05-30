d37bf1f899a4ef99d5da869b56fdd60530aad91c
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 11:49:11 -0400

    Update Figure_5g.R

--- Figure_5/Figure_5g.R
+++ Figure_5/Figure_5g.R
@@ -1,21 +1,23 @@
 library(dplyr)
 library(ggplot2)
 
-
 # load metadata
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
 #color 
-cols <- c('B_ISGhi'='#9ecae1',
-            'B_memory'='#283779',
-            'B_ABC'='#41b8ea',
-            'PCs'='#8856a7',
-            'B_transitional'='#756bb1',
-            'B_naive'='#1c9099')
+cols <- c('Tmem_TH2'= '#1c7b3d',
+            'Tmem_TH17'= '#3cb54a',
+            'Tmem_CM'= '#74c168',
+            'Tmem_HLA_DR'= '#a4de02ff',
+            'Tmem_ISGhi'= '#697d35',
+            'Tmem_CM_TOX'= '#edf8b1',
+            'Tmem_GzK_TH1_like'='#a8ddb5',
+            'Tmem_TEMRA'='#1c572b',
+            'Tmem_CM_SOX4'= '#7fcdbb')
             
 # subset to be plotted 
-subset_to_be_plotted <-  c('B_naive', 'B_transitional','B_ABC', 'B_memory','B_ISGhi','PCs')
+subset_to_be_plotted <- c("Tmem_CM", "Tmem_CM_SOX4","Tmem_CM_TOX", "Tmem_GzK_TH1_like","Tmem_TEMRA", "Tmem_TH2","Tmem_TH17","Tmem_HLA_DR","Tmem_ISGhi")
 
 BP_gp <- LifeSpan_ALL_MetaData %>% 
   mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
@@ -37,7 +39,7 @@ BP_gp <- LifeSpan_ALL_MetaData %>%
         axis.title.x = element_text(size=0),
         axis.ticks.x=element_blank(),
         axis.title.y = element_text(size=0),
-        legend.position = "none",
+        #legend.position = "none",
         plot.title = element_text(face='bold', color = 'black', size = 20, hjust = 0.5)) +
   ggtitle('Age groups')
 
