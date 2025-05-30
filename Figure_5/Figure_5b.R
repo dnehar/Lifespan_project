91baec873f4dae4704f6f39d2f61627b8b741433
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:33:35 -0400

    Update Figure_5b.R

--- Figure_5/Figure_5b.R
+++ Figure_5/Figure_5b.R
@@ -1,1 +1,14 @@
+library(dplyr)
+library(ggplot2)
+
+#color 
+cols <- c("CD4_T_Naive"="#193a1c",
+            "CD4_T_ISGhi"="#697d35",
+            "CD4_Tregs"="#137d82",
+            "CD4_T_Memory"="#1c572b",
+            'CD4_ISGhi'= '#697d35',
+            'CD4_T_ISGhi'='#697d35',
+            #CD4_CTL'= '#90aa3c',
+            'CD4_Naive'= '#193a1c')
+
 
