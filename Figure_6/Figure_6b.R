2faa95a8d0316992aa5405896933bcee378c4fbe
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:50:52 -0400

    Update Figure_6b.R

--- Figure_6/Figure_6b.R
+++ Figure_6/Figure_6b.R
@@ -1,1 +1,13 @@
+library(dplyr)
+library(ggplot2)
 
+#color 
+cols <- c("CD8_T_Naive"="#f37421",
+            'gd_Tcells'= '#80622f',
+            'CD8_Naive'= '#f37421',
+            'CD8_TEMRA'= '#d28529',
+            'CD8_GZMK'= '#fba919',
+            'CD8_MAIT'= '#fbb36a',
+            'CD8_cycling'='#7f7f7f',
+            'CD8_GD'= '#80622f',
+            'CD8_Naive_SOX4'= '#ffdeadff')
