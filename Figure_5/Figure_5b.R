4854b3794f0935e1d37673fe4f2309c6dffc7529
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-05-30 10:36:31 -0400

    Update Figure_5b.R

--- Figure_5/Figure_5b.R
+++ Figure_5/Figure_5b.R
@@ -10,5 +10,17 @@ cols <- c("CD4_T_Naive"="#193a1c",
             'CD4_T_ISGhi'='#697d35',
             #CD4_CTL'= '#90aa3c',
             'CD4_Naive'= '#193a1c')
+            
+
+# plot umap CD4 T cells
+
+p_umap_subset <- LifeSpan_ALL_MetaData %>% 
+dplyr::filter(Lineage %in% c('CD4_Tcells')) %>% 
+mutate(subset_simple_clustering = gsub(pattern = "CD4_CTL", replacement = "CD4_T_Memory", x = subset_simple_clustering)) %>%
+ggplot(aes(x=SC_umap1, y=SC_umap2,  color=subset_simple_clustering)) +
+geom_point(size=0.05) + #, alpha = 1
+scale_color_manual(values=cols) + 
+theme_void() 
+print(p_umap_subset)
 
 
