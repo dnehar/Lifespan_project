e290260ee22a50c54e969afacc78676c28195085
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 09:32:25 -0400

    Update Figure_6f.R

--- Figure_6/Figure_6f.R
+++ Figure_6/Figure_6f.R
@@ -1,11 +1,10 @@
+library(dplyr)
+library(ggplot2)
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
 
-#color 
-cols <- c('GD_T_SOX4'= '#56bbbf',           
-          'CD8_GD'= '#80622f')
-age_groups <- c("HI", "HC", "HY", "HO")
-my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
 
-# subset to be plotted 
-subset_to_be_plotted <- c('GD_T_SOX4','CD8_GD')
 
