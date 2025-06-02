76c42f8e2542096e8a9e1b0cb6bd45f9cc663933
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 09:34:58 -0400

    Update Figure_6f.R

--- Figure_6/Figure_6f.R
+++ Figure_6/Figure_6f.R
@@ -16,34 +16,30 @@ subset_to_be_plotted <- c('GD_T_SOX4','CD8_GD')
 
 
 
-  plt_age <- LifeSpan_ALL_MetaData %>% 
-    mutate(ReCluster = factor(Final_annotations)) %>% #, levels = ordered_SC
-    mutate(Groups = factor(Groups, levels = age_groups)) %>%
-    group_by(Groups, Names, ReCluster) %>%
-    summarise(n = n()) %>% #, Set = first(Set)
-    mutate(freq = n / sum(n) *100) %>%
-    ungroup() %>%
-    as.data.frame() %>%
-    
-    filter(ReCluster %in% subset_to_be_plotted) %>% 
-    ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = Groups)) +
-    geom_boxplot(outlier.shape = NA) +
-    geom_jitter(size = 0.2) +
-    theme_bw()  +  #THEME +
-    ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") + #label = "p.signif"
-    theme(legend.position = "none", 
-          strip.text = element_text(size = 14, face='bold')) +
-    facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) + 
-    
-    scale_fill_manual(values=cols) + #**
-    theme(axis.text.y=element_text(size=12, colour = 'black'), 
-          axis.text.x=element_text(size=12, colour = 'black'),
-          axis.title.x = element_text(face="bold", size=14, colour = 'black'),
-          axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
-          strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) + #    ylab('% PBMC') + xlab('Age groups')
-    ylab('% in PBMCs') + xlab('Age groups')
+BP_gp <- LifeSpan_ALL_MetaData %>% 
+  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
+  mutate(ReCluster = factor(Final_annotations)) %>%
+  group_by(Groups, ReCluster) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
   
-  plt_age
+  summarise(n = n()) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>% #head()
+  
+  ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = ReCluster)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_manual(values=cols) + #***
+  scale_x_discrete(limits=c("HI", "HC", "HY", "HO")) + #labels= labels
+  theme(axis.text.y=element_text(size=18), 
+        axis.text.x=element_text(size=18, angle = 0),
+        axis.title.x = element_text(size=0),
+        axis.ticks.x=element_blank(),
+        axis.title.y = element_text(size=0),
+        #legend.position = "none",
+        plot.title = element_text(face='bold', color = 'black', size = 20, hjust = 0.5)) +
+  ggtitle('Age groups')
 
+print(BP_gp)
 
 
