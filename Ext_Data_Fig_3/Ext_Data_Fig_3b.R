ad446e285f411ee09f030f56925edff7efbf8b1a
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 11:47:13 -0400

    Update Ext_Data_Fig_3b.R

--- Ext_Data_Fig_3/Ext_Data_Fig_3b.R
+++ Ext_Data_Fig_3/Ext_Data_Fig_3b.R
@@ -1,1 +1,8 @@
+library(dplyr)
+library(ggplot2)
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+pheno <- MetaData[['pheno']] %>% as.data.frame()
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
