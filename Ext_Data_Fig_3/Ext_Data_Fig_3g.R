0931cef48ae7c369b770cf73c025389952f4dfa8
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 11:56:42 -0400

    Update Ext_Data_Fig_3g.R

--- Ext_Data_Fig_3/Ext_Data_Fig_3g.R
+++ Ext_Data_Fig_3/Ext_Data_Fig_3g.R
@@ -7,19 +7,16 @@ pheno <- MetaData[['pheno']] %>% as.data.frame()
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
 #color 
-cols <- c('cDC2'= '#d84598',
-          'cDC1'= '#771215',
-          'AXL_DC'= '#a41e21',
-          'moDC'= '#ed2024',
-          'pDC'= '#a5a4a4')
+cols <- c('CD14_mo_ISGhi'= '#f15d64',
+          'CD14_mo'= '#f6a2a7',
+          'CD16_mo'= '#f9d3d7')
 
 
 age_groups <- c("HI", "HC", "HY", "HO")
 my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
 
 # subset to be plotted 
-subset_to_be_plotted <-  c('moDC','cDC1','cDC2', 'AXL_DC', 'pDC')
-
+subset_to_be_plotted <-  c('CD14_mo', 'CD14_mo_ISGhi', 'CD16_mo')
 
 plt_cor1 <- LifeSpan_ALL_MetaData %>%
   
