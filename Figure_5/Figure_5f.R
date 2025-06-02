49069fefb706e007c3677e8faac59c4bbaeceed9
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 14:36:10 -0400

    Update Figure_5f.R

--- Figure_5/Figure_5f.R
+++ Figure_5/Figure_5f.R
@@ -5,24 +5,26 @@ library(ggplot2)
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
-
 #color 
-cols <- c("CD4_T_Naive"="#193a1c",
-            "CD4_T_ISGhi"="#697d35",
-            "CD4_Tregs"="#137d82",
-            "CD4_T_Memory"="#1c572b",
-            'CD4_ISGhi'= '#697d35',
-            'CD4_T_ISGhi'='#697d35',
-            #CD4_CTL'= '#90aa3c',
-            'CD4_Naive'= '#193a1c')
+cols <- c('Tmem_TH2'= '#1c7b3d',
+            'Tmem_TH17'= '#3cb54a',
+            'Tmem_CM'= '#74c168',
+            'Tmem_HLA_DR'= '#a4de02ff',
+            'Tmem_ISGhi'= '#697d35',
+            'Tmem_CM_TOX'= '#edf8b1',
+            'Tmem_GzK_TH1_like'='#a8ddb5',
+            'Tmem_TEMRA'='#1c572b',
+            'Tmem_CM_SOX4'= '#7fcdbb')
             
+# subset to be plotted 
+subset_to_be_plotted <- c("Tmem_CM", "Tmem_CM_SOX4","Tmem_CM_TOX", "Tmem_GzK_TH1_like","Tmem_TEMRA", "Tmem_TH2","Tmem_TH17","Tmem_HLA_DR","Tmem_ISGhi")
+
 
 # plot umap CD4 T cells
 
 p_umap_subset <- LifeSpan_ALL_MetaData %>% 
-dplyr::filter(Lineage %in% c('CD4_Tcells')) %>% 
-mutate(subset_simple_clustering = gsub(pattern = "CD4_CTL", replacement = "CD4_T_Memory", x = subset_simple_clustering)) %>%
-ggplot(aes(x=SC_umap1, y=SC_umap2,  color=subset_simple_clustering)) +
+dplyr::filter(Final_annotations %in% subset_to_be_plotted) %>% 
+ggplot(aes(x=SC_umap1, y=SC_umap2,  color=Final_annotations)) +
 geom_point(size=0.05) + #, alpha = 1
 scale_color_manual(values=cols) + 
 theme_void() 
