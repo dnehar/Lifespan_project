7d284d10606e2b7b0b2bd0969b07bc4f6f358486
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 09:31:33 -0400

    Update Figure_6f.R

--- Figure_6/Figure_6f.R
+++ Figure_6/Figure_6f.R
@@ -1,1 +1,11 @@
 
+
+#color 
+cols <- c('GD_T_SOX4'= '#56bbbf',           
+          'CD8_GD'= '#80622f')
+age_groups <- c("HI", "HC", "HY", "HO")
+my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
+
+# subset to be plotted 
+subset_to_be_plotted <- c('GD_T_SOX4','CD8_GD')
+
