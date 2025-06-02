08aa443bef61613433a2e750ad38b7d4ff2b9040
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 11:17:47 -0400

    Update Ext_Data_Fig_1a.R

--- Ext_Data_Fig_1/Ext_Data_Fig_1a.R
+++ Ext_Data_Fig_1/Ext_Data_Fig_1a.R
@@ -1,1 +1,20 @@
+library(dplyr)
+library(ggplot2)
 
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+pheno <- MetaData[['meta_small']] %>% as.data.frame()
+
+my_col <- c('F'= '#a8ddb5', M='#feb24c')
+
+pPC <- pheno %>% group_by(Groups, Sex) %>% summarise(n = n()) %>%   mutate(freq = n / sum(n) *100) %>%  
+  mutate(Groups = factor(Groups, levels = age_groups)) %>%
+  ggplot(aes(x = '', y = freq, fill = Sex, group = Sex)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  facet_wrap(.~Groups, scales = "free_y", nrow = 2) + 
+  scale_fill_manual(values=my_col) + #***
+  #scale_x_discrete(limits=c("HY_F","HO_F",'HY_M','HO_M')) + #labels= labels
+  theme_void()  # remove background, grid, numeric labels
+
+print(pPC)
