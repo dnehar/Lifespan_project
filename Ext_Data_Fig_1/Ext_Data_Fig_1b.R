c02273e16b6aaf4d26893d3c7041d396fac9671e
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-02 11:19:58 -0400

    Update Ext_Data_Fig_1b.R

--- Ext_Data_Fig_1/Ext_Data_Fig_1b.R
+++ Ext_Data_Fig_1/Ext_Data_Fig_1b.R
@@ -3,18 +3,29 @@ library(ggplot2)
 
 # load metadata
 MetaData <- readRDS('./pbmcs_v1.rds')
-pheno <- MetaData[['meta_small']] %>% as.data.frame()
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
-my_col <- c('F'= '#a8ddb5', M='#feb24c')
-
-pPC <- pheno %>% group_by(Groups, Sex) %>% summarise(n = n()) %>%   mutate(freq = n / sum(n) *100) %>%  
-  mutate(Groups = factor(Groups, levels = age_groups)) %>%
-  ggplot(aes(x = '', y = freq, fill = Sex, group = Sex)) +
-  geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0) +
-  facet_wrap(.~Groups, scales = "free_y", nrow = 2) + 
-  scale_fill_manual(values=my_col) + #***
-  #scale_x_discrete(limits=c("HY_F","HO_F",'HY_M','HO_M')) + #labels= labels
-  theme_void()  # remove background, grid, numeric labels
-
-print(pPC)
+  MetaData <- LifeSpan_ALL_MetaData
+  Number <- data.frame(MetaData %>% count(Names, Groups)) #Gender,
+  Number %>% group_by(Groups) %>% summarize(Mean= mean(n), SD=sd(n)) -> LNB
+  
+  #head(LNB)
+  px <- ggplot(data=Number, aes(x=Groups, y=n, fill=Groups)) +
+    geom_violin(aes(fill = Groups), trim = TRUE)  +
+    stat_summary(fun.data = "mean_sdl") +
+    geom_dotplot(binaxis='y', stackdir='center',stackratio=1.5, dotsize=1) + #,binwidth = 20
+    #+ coord_flip() 
+    scale_fill_manual(values=col_age_gp) +
+    ylab("number of cells") +
+    xlab("age groups") +
+    scale_x_discrete(limits = c("HI","HC","HY","HO"))+
+    theme(legend.position="none", 
+          axis.text.x=element_text(size=18), 
+          axis.text.y=element_text(size=18), 
+          axis.title.x = element_text(face="bold", size=18),
+          axis.title.y = element_text(face="bold", size=18),
+          plot.title = element_text(hjust = 0.5,face='bold',size=14),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(fill=NA, color = 'black', size=1))
+  print(px)
