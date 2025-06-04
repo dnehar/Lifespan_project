8c9857b0ad01e1401294283af4dfc2e2d38ac77d
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-04 10:52:33 -0400

    Update Ext_Data_Fig_7b.R

--- Ext_Data_Fig_7/Ext_Data_Fig_7b.R
+++ Ext_Data_Fig_7/Ext_Data_Fig_7b.R
@@ -1,1 +1,82 @@
+library(dplyr)
+library(ggplot2)
+
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+pheno <- MetaData[['pheno']] %>% as.data.frame()
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+ cols <- c('CD4_Naive'= '#193a1c',
+            'CD4_Naive_SOX4'='#a4de02ff',
+          'CD8_Naive'= '#f37421',
+           'CD8_Naive_SOX4'= '#ffdeadff')
+
+# 1- naive CD4 T cells  
+
+subset_to_be_plotted <-  c('CD4_Naive', 'CD4_Naive_SOX4')
+
+plt_cor1 <- LifeSpan_ALL_MetaData %>%
+  
+  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
+  mutate(ReCluster = factor(Final_annotations)) %>% #*****
+  mutate(Age_days = Age_months*30) %>% 
+  group_by(Groups, Names,Age_months,Age_days, ReCluster) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>%   
+  summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>%
+  ggplot(aes(x = Age_months, y = freq, fill=ReCluster)) +
+  geom_smooth(method = "lm", aes(color=ReCluster)) + #, color = c('#f37421ff','#ffdeadff')
+  geom_point(aes(shape = Groups, color=ReCluster)) +
+  scale_fill_manual(values=cols) + 
+  scale_color_manual(values = cols)+
+  ggpubr::stat_cor() +
+  theme_bw() +
+  theme(legend.position = "none", 
+        strip.text = element_text(size = 14)) +
+  facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) +
+  theme(axis.text.y=element_text(size=12, colour = 'black'), 
+        axis.text.x=element_text(size=12, colour = 'black'),
+        axis.title.x = element_text(face="bold", size=14, colour = 'black'),
+        axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
+        strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) +#    ylab('% PBMC') + xlab('Age groups') #    ylab('% PBMC') + xlab('Age groups'
+  ylab('% total CD4 T cells') + xlab('Age (months)')
+plt_cor1
+
+
+# 2- naive CD8 T cells  
+
+subset_to_be_plotted <-  c('CD8_Naive', 'CD8_Naive_SOX4')
+
+plt_cor2 <- LifeSpan_ALL_MetaData %>%
+  
+  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
+  mutate(ReCluster = factor(Final_annotations)) %>% #*****
+  mutate(Age_days = Age_months*30) %>% 
+  group_by(Groups, Names,Age_months,Age_days, ReCluster) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>%   
+  summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>%
+  ggplot(aes(x = Age_months, y = freq, fill=ReCluster)) +
+  geom_smooth(method = "lm", aes(color=ReCluster)) + #, color = c('#f37421ff','#ffdeadff')
+  geom_point(aes(shape = Groups, color=ReCluster)) +
+  scale_fill_manual(values=cols) + 
+  scale_color_manual(values = cols)+
+  ggpubr::stat_cor() +
+  theme_bw() +
+  theme(legend.position = "none", 
+        strip.text = element_text(size = 14)) +
+  facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) +
+  theme(axis.text.y=element_text(size=12, colour = 'black'), 
+        axis.text.x=element_text(size=12, colour = 'black'),
+        axis.title.x = element_text(face="bold", size=14, colour = 'black'),
+        axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
+        strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) +#    ylab('% PBMC') + xlab('Age groups') #    ylab('% PBMC') + xlab('Age groups'
+  ylab('% total CD8 T cells') + xlab('Age (months)')
+plt_cor2
 
