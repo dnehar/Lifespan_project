8fc8a955130005a9e5c26b756efeb743e70feed5
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2025-06-04 10:45:25 -0400

    Update Ext_Data_Fig_7a.R

--- Ext_Data_Fig_7/Ext_Data_Fig_7a.R
+++ Ext_Data_Fig_7/Ext_Data_Fig_7a.R
@@ -1,1 +1,18 @@
+library(dplyr)
+library(ggplot2)
+
+
+# load metadata
+MetaData <- readRDS('./pbmcs_v1.rds')
+pheno <- MetaData[['pheno']] %>% as.data.frame()
+LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+
+#color 
+ cols <- c('CD4_Naive'= '#193a1c',
+            'CD4_Naive_SOX4'='#a4de02ff')
+
+# subset to be plotted 
+subset_to_be_plotted <-  c('CD4_Naive', 'CD4_Naive_SOX4')
+
+
 
