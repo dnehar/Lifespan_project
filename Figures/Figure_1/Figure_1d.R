594ee04f0f991fad6f7d8de9edbbfb7b4b4282be
Djamel Nehar-Belaid <djamel.nehar-belaid@jax.org>
2026-03-13 15:45:08 -0400

    modified script

--- Figures/Figure_1/Figure_1d.R
+++ Figures/Figure_1/Figure_1d.R
@@ -1,30 +1,43 @@
-library(dplyr)
-library(ggplot2)
-  
+
+library(dplyr); library(ggplot2); 
+
+
+cols <- c(
+  # Groups
+  "Infants" = "#0072B2",
+  "Child" = "#56B4E9",
+  "Adolescent" = "#009E73", 
+  "Young" = "#F0E442",
+  "Middle_aged" = "#E69F00",
+  "Older" ="#D55E00",
+  "Oldest_old" = "#CC79A7"
+)
+
 # load metadata  
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
-# colors 
-col_age_gp <- c('HI'='#99d8c9',
-                'HC'='#9ecae1',
-                'HY'='#fec44f',
-                'HO'='#bcbddc')
 
-# PCA
 proportions <- LifeSpan_ALL_MetaData %>%
-
-  group_by(Names, Final_annotations) %>%
-  summarise(n = n(), Groups = first(Groups), Sex = first(Sex), Ethnicity = first(Ethnicity_v2), Age_months = first(Age_months), Lineage = first(Lineage_v2)) %>%
+  #filter(Groups %in% c('HO','HY','HC')) %>%  #& Lineage %in% c('Monocytes','DCs')) %>%
+  #filter(!sample_id %in% c('sample6','sample12','sample13',
+  #                        'sample14','sample15','sample47','donor9')) %>% #
+  mutate(ReCluster = factor(LS_L2)) %>%
+  
+  #group_by(Names, Simple_Clustering) %>%
+  group_by(sample_id, ReCluster) %>%
+  
+  summarise(n = n(), Groups = first(Age_groups), Age_months = first(Age_in_yrs),  Lineage = first(lifespan_L1)) %>%
   mutate(freq = n / sum(n) *100) %>%
   ungroup() %>%
   as.data.frame()
 
 
 t.proportions <- proportions %>%
-  reshape2::dcast(Names + Groups + Sex + Ethnicity + Age_months ~ Final_annotations, value.var = "freq", fill = 0)
+  reshape2::dcast(sample_id + Groups + Age_months ~ ReCluster, value.var = "freq", fill = 0)
+head(t.proportions)
 
-pca <- prcomp(t.proportions[,6:ncol(t.proportions)], center = T)
+pca <- prcomp(t.proportions[,4:ncol(t.proportions)], center = T) #***
 
 d  <- round(pca$sdev^2/sum(pca$sdev^2)*100, digits=1)
 xl <- sprintf("PC 1: %.1f %%", d[1])
@@ -32,27 +45,29 @@ yl <- sprintf("PC 2: %.1f %%", d[2])
 zl <- sprintf("PC 3: %.1f %%", d[3])
 
 
+# ├├ plot PC1 - PC2 ####
 t.proportions <- cbind(t.proportions, data.frame("PC1" = as.numeric(pca$x[,1]), "PC2"= as.numeric(pca$x[,2]), "PC3"= as.numeric(pca$x[,3])))
-t.proportions$Groups <- factor( t.proportions$Groups, levels = c("HI","HC","HY","HO"))
+t.proportions$Groups <- factor(t.proportions$Groups, levels = age_groups)
 head(t.proportions)
 
-plt_pca <- t.proportions %>% 
+plt_pca_L2 <- t.proportions %>% 
   ggplot(aes(x = PC1, y = PC2, group = Groups, color = Groups)) +
-  geom_point(aes(color = Groups, shape = Sex), size = 5) +
+  geom_point(aes(color = Groups), size = 3) + #, shape = Sex
   theme_bw() +
   coord_fixed(ratio = 1) +
-  scale_fill_manual(values=col_age_gp) + 
-  scale_color_manual(values=col_age_gp) + 
+  scale_fill_manual(values=cols) + 
+  scale_color_manual(values=cols) + 
   theme(axis.text.y=element_text(size=18), 
         axis.text.x=element_text(size=18),
         axis.title.x = element_text(face="bold", size=20),
         axis.title.y = element_text(face="bold", size=20)) +
-  ggtitle("PBMCs")  +
-
   
-  stat_ellipse(type = "norm", linetype = 1, level = 0.8)+ 
+  stat_ellipse(type = "norm", linetype = 1, level = 0.4)+ 
   labs(x=xl,y=yl) +
-  ggtitle("PBMCs ")  +
+  ggtitle("PBMCs - Level 2 (n=18) ")  +
   theme(plot.title = element_text(hjust = 0.5, vjust = 2, size=20, face="bold"))
-print(plt_pca)
+plt_pca_L2
 
+
+ggsave("./Figure_2026/LS_pca_plot_L2_012562026.pdf", plt_pca_L2,
+       width=3, height=3,  units="in", scale=3)
