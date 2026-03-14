7192d71b0e007a49e35104f71939a9146e9b8d0a
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-14 12:08:59 -0400

    Clean up Figure_1e.R by removing unused code
    Removed commented-out filter and group_by lines for clarity.

--- Figures/Figure_1/Figure_1e.R
+++ Figures/Figure_1/Figure_1e.R
@@ -6,31 +6,21 @@ LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
 
 proportions <- LifeSpan_ALL_MetaData %>%
-  #filter(Groups %in% c('HO','HY','HC')) %>%  #& Lineage %in% c('Monocytes','DCs')) %>%
-  #filter(!sample_id %in% c('sample6','sample12','sample13',
-  #                        'sample14','sample15','sample47','donor9')) %>% #
   mutate(ReCluster = factor(LS_L2)) %>%
-  
-  #group_by(Names, Simple_Clustering) %>%
   group_by(sample_id, ReCluster) %>%
-  
   summarise(n = n(), Groups = first(Age_groups), Age_months = first(Age_in_yrs),  Lineage = first(lifespan_L1)) %>%
   mutate(freq = n / sum(n) *100) %>%
   ungroup() %>%
   as.data.frame()
 
-
 t.proportions <- proportions %>%
   reshape2::dcast(sample_id + Groups + Age_months ~ ReCluster, value.var = "freq", fill = 0)
 head(t.proportions)
-
 pca <- prcomp(t.proportions[,4:ncol(t.proportions)], center = T) #***
-
 d  <- round(pca$sdev^2/sum(pca$sdev^2)*100, digits=1)
 xl <- sprintf("PC 1: %.1f %%", d[1])
 yl <- sprintf("PC 2: %.1f %%", d[2])
 zl <- sprintf("PC 3: %.1f %%", d[3])
-
 
 # ├ ├Plot PC contributuons ####
 contrib <- as.data.frame(pca$rotation)
