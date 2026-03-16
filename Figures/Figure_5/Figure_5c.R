2cb233f931a571cabc12e3b7a0a2c8923133ecda
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-16 14:46:43 -0400

    Update UMAP plotting with new data and save option
    Refactored UMAP plotting code to use new data structure and added save functionality.

--- Figures/Figure_5/Figure_5c.R
+++ Figures/Figure_5/Figure_5c.R
@@ -1,21 +1,22 @@
+
 library(dplyr)
 library(ggplot2)
 
-# load metadata
-MetaData <- readRDS('./pbmcs_v1.rds')
-LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+LS_list <- readRDS("./umaps_coordinates.rds")
+df <- LS_list[['Tregs']]
 
-#color 
-cols <- c('naive_Tregs'= '#137d82',
-            'mem_Tregs'= '#56bbbf')
-# subset to be plotted 
-subset_to_be_plotted <- c("naive_Tregs", "mem_Tregs")
-           
-# plot umap B cells
-p_umap_subset <- LifeSpan_ALL_MetaData %>% 
- dplyr::filter(subset_simple_clustering %in% c('CD4_Tregs')) %>% 
-ggplot(aes(x=X_umap1, y=X_umap2,  color=Final_annotations)) +
-geom_point(size=0.5) + #, alpha = 1
-scale_color_manual(values=cols) + 
-theme_void() 
-print(p_umap_subset)
+p <- df %>%
+    ggplot(aes(x = SC_umap1, y = SC_umap2, color = Final_annotations)) +
+    geom_point(size = point_size) +
+    scale_color_manual(values = cols_vec, drop = FALSE) +
+    theme_void() +
+    guides(color = guide_legend(override.aes = list(size = 3))) +
+    labs(title = subset_name, color = "Final annotations")
+  
+  # Save if requested
+  if (!is.null(save_path)) {
+    ggsave(filename = save_path, plot = p, width = width, height = height, dpi = dpi)
+    message(sprintf("Saved plot to: %s", save_path))
+  }
+  
+return(p)
