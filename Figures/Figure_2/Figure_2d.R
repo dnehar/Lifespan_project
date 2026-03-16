7e582711a559e63e5941d405dcfeb5dce2b66c7d
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-16 11:17:17 -0400

    Refactor box plot code and save output as PDF
    Updated the box plot code for lineage frequencies by age groups, added age group comparisons, and saved the plot as a PDF.

--- Figures/Figure_2/Figure_2d.R
+++ Figures/Figure_2/Figure_2d.R
@@ -1,55 +1,52 @@
-library(dplyr)
-library(ggplot2)
+library(dplyr); library(ggplot2); 
 
+cols <- c(  "moDC" = "#ed2024",
+            "cDC1" = "#771215",
+            "cDC2" = "#d84598",
+            "AXL_DC" = "#a41e21",
+            "pDC" = "#a5a4a4")
 
-# load metadata
+# load metadata and sample informatiom
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
-#color 
-cols <- c('cDC2'= '#d84598',
-          'cDC1'= '#771215',
-          'AXL_DC'= '#a41e21',
-          'moDC'= '#ed2024',
-          'pDC'= '#a5a4a4')
+age_groups <- c('Infants', 'Child','Adolescent', 'Young', 'Middle_aged', 'Older', 'Oldest_old')
+my_comparisons <- list (c('Infants', 'Child'),
+                        c('Child','Adolescent'),
+                        c('Adolescent', 'Young'),
+                        c('Young', 'Middle_aged'),
+                        c('Middle_aged', 'Older'),
+                        c('Older', 'Oldest_old'))
 
-
-age_groups <- c("HI", "HC", "HY", "HO")
-my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
-
-# subset to be plotted 
-subset_to_be_plotted <-  c('moDC','cDC1','cDC2', 'AXL_DC', 'pDC')
-
-# plot box plot - age groups 
-
-  plt_age <- LifeSpan_ALL_MetaData %>% 
-    mutate(ReCluster = factor(Final_annotations)) %>% #, levels = ordered_SC
-    mutate(Groups = factor(Groups, levels = age_groups)) %>%
-    group_by(Groups, Names, ReCluster) %>%
-    filter(ReCluster %in% subset_to_be_plotted) %>% 
-    summarise(n = n()) %>% #, Set = first(Set)
-    mutate(freq = n / sum(n) *100) %>%
-    ungroup() %>%
-    as.data.frame() %>%
-    
-    ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = Groups)) +
-    geom_boxplot(outlier.shape = NA) +
-    geom_jitter(size = 0.2) +
-    theme_bw()  +  #THEME +
-    #ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") +
-    ggpubr::stat_compare_means(comparisons = my_comparisons, method = "t.test") + #label = "p.signif"
-    #ggpubr::stat_compare_means(comparisons = my_comparisons, label = "p.signif", hide.ns = F, vjust = 0.5) + 
-    theme(legend.position = "none", 
-          strip.text = element_text(size = 14, face='bold')) +
-    facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) + 
-    
-    scale_fill_manual(values=cols) + #**
-    theme(axis.text.y=element_text(size=12, colour = 'black'), 
-          axis.text.x=element_text(size=12, colour = 'black'),
-          axis.title.x = element_text(face="bold", size=14, colour = 'black'),
-          axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
-          strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) + #    ylab('% PBMC') + xlab('Age groups')
-    ylab('% in DCs') + xlab('Age groups')
+box_plot_lineage <- LifeSpan_ALL_MetaData %>% 
+  mutate(ReCluster = factor(LS_L4, levels = order_LS_L4)) %>%
+  mutate(Groups = factor(Age_groups, levels = age_groups)) %>%
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
+  group_by(Groups, sample_id, ReCluster) %>%
+  summarise(n = n()) %>% #, Set = first(Set)
+  mutate(freq = n / sum(n) *100) %>%
+  ungroup() %>%
+  as.data.frame() %>%
+  ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = Groups)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(size = 0.2) +
+  theme_bw()  +  #THEME +
+  ggpubr::stat_compare_means(comparisons = my_comparisons,  label = "p.format", hide.ns = T, vjust = 0.5) + 
+  theme(legend.position = "none", 
+        strip.text = element_text(size = 10, face='bold')) +
+  facet_wrap(.~ReCluster, scales = "free_y", nrow = 1) + 
   
-  plt_age
-  
+  scale_fill_manual(values=cols) + #**
+  theme(axis.text.y=element_text(size=12, colour = 'black'), 
+        axis.text.x=element_text(size=12, colour = 'black',angle = 90),
+        axis.title.x = element_text(face="bold", size=14, colour = 'black'),
+        axis.title.y = element_text(face="bold", size=14, colour = 'black'), 
+        strip.text.x = element_text(size = 14, face ='bold', colour = 'black')) + #    ylab('% PBMC') + xlab('Age groups')
+  ylab('% of lineage') + xlab('Age groups')
+
+box_plot_lineage
+
+# --- Save figure as PDF ---
+# Note: corrected plot object name from box_plot_pbmc_L2 to box_plot_pbmc_L4
+ggsave("./Boxplot_DCs_in_lineage_03132026.pdf", box_plot_lineage,
+       width = 4.2, height = 3, units = "in", scale = 3)
