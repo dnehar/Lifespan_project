22bcb5956b60efe7626345e104a8b39202cbc814
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-16 14:47:24 -0400

    Refactor Figure 5g.R to use new data structure

--- Figures/Figure_5/Figure_5g.R
+++ Figures/Figure_5/Figure_5g.R
@@ -1,47 +1,22 @@
+
 library(dplyr)
 library(ggplot2)
 
-# load metadata
-MetaData <- readRDS('./pbmcs_v1.rds')
-LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+LS_list <- readRDS("./umaps_coordinates.rds")
+df <- LS_list[['T_helpers']]
 
-#color 
-cols <- c('Tmem_TH2'= '#1c7b3d',
-            'Tmem_TH17'= '#3cb54a',
-            'Tmem_CM'= '#74c168',
-            'Tmem_HLA_DR'= '#a4de02ff',
-            'Tmem_ISGhi'= '#697d35',
-            'Tmem_CM_TOX'= '#edf8b1',
-            'Tmem_GzK_TH1_like'='#a8ddb5',
-            'Tmem_TEMRA'='#1c572b',
-            'Tmem_CM_SOX4'= '#7fcdbb')
-            
-# subset to be plotted 
-subset_to_be_plotted <- c("Tmem_CM", "Tmem_CM_SOX4","Tmem_CM_TOX", "Tmem_GzK_TH1_like","Tmem_TEMRA", "Tmem_TH2","Tmem_TH17","Tmem_HLA_DR","Tmem_ISGhi")
-
-BP_gp <- LifeSpan_ALL_MetaData %>% 
-  mutate(Groups = factor(Groups, levels = c("HI", "HC", "HY", "HO"))) %>%
-  mutate(ReCluster = factor(Final_annotations)) %>%
-  group_by(Groups, ReCluster) %>%
-  filter(ReCluster %in% subset_to_be_plotted) %>% 
+p <- df %>%
+    ggplot(aes(x = SC_umap1, y = SC_umap2, color = Final_annotations)) +
+    geom_point(size = point_size) +
+    scale_color_manual(values = cols_vec, drop = FALSE) +
+    theme_void() +
+    guides(color = guide_legend(override.aes = list(size = 3))) +
+    labs(title = subset_name, color = "Final annotations")
   
-  summarise(n = n()) %>% #, Set = first(Set)
-  mutate(freq = n / sum(n) *100) %>%
-  ungroup() %>%
-  as.data.frame() %>% #head()
+  # Save if requested
+  if (!is.null(save_path)) {
+    ggsave(filename = save_path, plot = p, width = width, height = height, dpi = dpi)
+    message(sprintf("Saved plot to: %s", save_path))
+  }
   
-  ggplot(aes(x = Groups, y = freq, fill = ReCluster, group = ReCluster)) +
-  geom_bar(stat = "identity", color = "black") +
-  scale_fill_manual(values=cols) + #***
-  scale_x_discrete(limits=c("HI", "HC", "HY", "HO")) + #labels= labels
-  theme(axis.text.y=element_text(size=18), 
-        axis.text.x=element_text(size=18, angle = 0),
-        axis.title.x = element_text(size=0),
-        axis.ticks.x=element_blank(),
-        axis.title.y = element_text(size=0),
-        #legend.position = "none",
-        plot.title = element_text(face='bold', color = 'black', size = 20, hjust = 0.5)) +
-  ggtitle('Age groups')
-
-BP_gp
-
+return(p)
