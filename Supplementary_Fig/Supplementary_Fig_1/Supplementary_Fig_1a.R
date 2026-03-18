090fd2fc9136ef315aa8223d50256757649ef789
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-18 08:29:57 -0400

    Update script for Supplementary Fig. 1a to pie chart

--- Supplementary_Fig/Supplementary_Fig_1/Supplementary_Fig_1a.R
+++ Supplementary_Fig/Supplementary_Fig_1/Supplementary_Fig_1a.R
@@ -1,11 +1,9 @@
 
 # =============================================================================
-# Supplementary Fig. 1a  — Scatter plots of PBMCs subset frequencies vs. age in Infants (Level 1 - clustering)
+# Supplementary Fig. 1a  — Pie char showing Biological sex information across age groups
 #
-# This script computes frequencies of nine PBMC cell subtypes
-# as scatter plots with linear regression fits and Pearson correlation coefficients.
 # Input:  pbmcs_v1.rds  — available at dnehar/Lifespan_project/pbmcs_v1.rds
-# Output: ./corplot_PBMCs_cells_LS_L1_03132026.pdf
+# Output: ./Piechart_Platform_ageGroups_03182026.pdf
 # =============================================================================
 
 library(dplyr); library(ggplot2)
@@ -15,62 +13,48 @@ library(dplyr); library(ggplot2)
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
 
-
-# --- Color palette  cols <- c(
-   "CD4_Tcells" = "#193a1c",
-   "CD8_Tcells" = "#f37421",
-   "gd_Tcells" = "#80622f",
-   "NK_cells" = "#fee000",
-   "B_cells" = "#1c9099",
-   "PCs" = "#8856a7",
-   "monocytes" = "#f6a2a7",
-   "DCs" = "#ed2024",
-   "HSPC" = "#b0479a")
-
 age_groups <- c('Infants', 'Child','Adolescent', 'Young', 'Middle_aged', 'Older', 'Oldest_old')
-my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
 
-p_corr_pbmc_L1 <- LifeSpan_ALL_MetaData %>%
-  
-  mutate(ReCluster = factor(LS_L1, levels = order_LS_L1)) %>% #***
-  mutate(Groups = factor(Age_groups, levels = age_groups)) %>%
-  group_by(Groups, sample_id, Age_in_yrs, ReCluster) %>%
-  summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
-  #summarise(n = n()) %>% #, Set = first(Set)
-  mutate(freq = n / sum(n) *100) %>%
-  ungroup() %>%
-  as.data.frame() %>%
-  #filter(ReCluster %in% subset_to_be_plotted) %>%  
-  ggplot(aes(x = Age_in_yrs, y = freq, fill=ReCluster)) +
-  geom_point(shape = 21, aes(fill = ReCluster), color = "black", size = 3, stroke = 0.5) + #stroke: thickness of the border
-  geom_smooth(method = "lm", aes(color=ReCluster)) + #, color = c('#f37421ff','#ffdeadff')
-  #geom_smooth(method = "lm", formula = y ~ poly(x, 2), aes(color=ReCluster)) +
-  scale_fill_manual(values=cols) + #**** 
-  scale_color_manual(values = cols)+ #****
-  ggpubr::stat_cor() +
-  #theme_bw() +
-  theme(legend.position = "none", 
-        strip.text = element_text(size = 13, face ='bold')) +
-  facet_wrap(.~ReCluster, scales = "free_y", nrow = 2) + #***
-  theme_bw() +
-  # -------- ALL TEXT IN BLACK 
-theme(
-  legend.position = "none",
-  
-  # Facet strip labels
-  strip.text = element_text(size = 13, face = "bold", colour = "black"),
-  
-  # Axis tick labels
-  axis.text.y  = element_text(size = 16, colour = "black"),
-  axis.text.x  = element_text(size = 16, colour = "black"),
-  
-  # Axis titles
-  axis.title.x = element_text(face = "bold", size = 18, colour = "black"),
-  axis.title.y = element_text(face = "bold", size = 18, colour = "black")
-) +
-ylab('% PBMC')  + xlab('Age (years)')
+sample_info <- read.csv('./sample_info_01082026.csv')                          
+col_plat <- c('#a8dde3','#fbb36a')
 
-p_corr_pbmc_L1
+p_plaform <- donor_table %>%
+  group_by(Age_groups, Platform) %>%
+  summarise(n = n(), .groups = "drop_last") %>%
+  mutate(freq = n / sum(n) * 100) %>%
+  mutate(Age_groups = factor(Age_groups, levels = age_groups)) %>%
+  ggplot(aes(x = "", y = freq, fill = Platform)) +
+  
+  # Pie slices
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  
+  # --- NEW: Add counts in the middle of each slice ---
+  geom_text(aes(label = n),
+            position = position_stack(vjust = 0.5),
+            size = 4, color = "black", fontface = "bold") +
+  
+  coord_polar("y", start = 0) +
+  
+  facet_wrap(
+    . ~ Age_groups,
+    scales = "free_y",
+    nrow = 2,
+    labeller = labeller(Age_groups =
+                          c(
+                            'Infants'='Infants (n=36): 2m-2y',
+                            'Child'='Child (n=26): 2y-12y',
+                            'Adolescent'='Adolescent (n=20): 12y-18y',
+                            'Young'='Young (n=24): 18y-40y',
+                            'Middle_aged'='Middle_aged (n=16): 40y-65y',
+                            'Older'='Older (n=33): 65y-85y',
+                            'Oldest_old'='Oldest_old (n=12): 85y-105y'
+                          ))
+  ) +
+  scale_fill_manual(values = col_plat) +
+  theme_void() +
+  theme(strip.text = element_text(size = 14, face = "bold"))
 
-ggsave("./corplot_PBMCs_cells_LS_L1_03132026.pdf", p_corr_pbmc_L1,
-       width=5, height=2,  units="in", scale=3)
+p_plaform
+
+ggsave("./Piechart_Platform_ageGroups_03182026.pdf", p_plaform,
+       width=6.2, height=2.2,  units="in", scale=3)
