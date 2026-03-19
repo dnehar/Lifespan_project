88480e2736462ff9279403cb1cf0aa0e1587658f
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-19 11:14:41 -0400

    Update Supplementary_Fig_6b.R

--- Supplementary_Fig/Supplementary_Fig_6/Supplementary_Fig_6b.R
+++ Supplementary_Fig/Supplementary_Fig_6/Supplementary_Fig_6b.R
@@ -38,7 +38,7 @@ cols <- c(
 
 p_corr_lineage <- LifeSpan_ALL_MetaData %>%
   
-  mutate(ReCluster = factor(LS_L3, levels=p_corr_pbmc_L4)) %>% #***
+  mutate(ReCluster = factor(LS_L3, levels=subset_to_be_plotted)) %>% #***
   mutate(Groups = factor(Age_groups, levels = age_groups)) %>%
   group_by(Groups, sample_id, Age_in_yrs, ReCluster) %>%
   filter(ReCluster %in% subset_to_be_plotted) %>%  
