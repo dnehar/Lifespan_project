d5a453f8f497425c48bc5da724a8a6dcdf949f18
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-03-19 09:52:07 -0400

    Update age groups and plotting parameters in Figure 5e

--- Figures/Figure_5/Figure_5e.R
+++ Figures/Figure_5/Figure_5e.R
@@ -7,6 +7,7 @@ library(ggstream)
 # load metadata
 MetaData <- readRDS('./pbmcs_v1.rds')
 LifeSpan_ALL_MetaData <- MetaData[['meta_small']] %>% as.data.frame()
+pheno <- MetaData[['pheno']] %>% as.data.frame()
 
 
 #color 
@@ -14,33 +15,36 @@ cols <- c('naive_Tregs'= '#137d82',
             'mem_Tregs'= '#56bbbf')
             
 
-age_groups <- c("HI", "HC", "HY", "HO")
+age_groups <- c('Infants', 'Child', 'Adolescent', 'Young', 'Middle_aged', 'Older', 'Oldest_old')
 my_comparisons <- combn(age_groups,2, FUN = list, simplify = T)
 
-# subset to be plotted 
+ordered_names <- unique(pheno$sample_id)
 
+
+# subset to be plotted 
 subset_to_be_plotted <- c("naive_Tregs", "mem_Tregs")
 
 BP <- LifeSpan_ALL_MetaData %>% 
-  mutate(ReCluster = factor(Final_annotations)) %>% #*****
-  group_by(Groups,  Names, ReCluster) %>%
-  summarise(n = n(), Age_months = first(Age_months), Gender = first(Sex)) %>% #, Set = first(Set)
+  mutate(Groups = factor(Age_groups, levels = age_groups)) %>%
+  mutate(ReCluster = factor(LS_L4)) %>% #*****
+  filter(ReCluster %in% subset_to_be_plotted) %>% 
+  group_by(Groups, sample_id, Age_in_yrs, ReCluster) %>%
+  summarise(n = n(), Age_months = first(Age_in_yrs)) %>% #, Set = first(Set)
   mutate(freq = n / sum(n)*100) %>%
   ungroup() %>%
   as.data.frame() %>% #head()
-  filter(ReCluster %in% subset_to_be_plotted ) %>% 
-  ggplot(aes(x = Names, y = freq, fill = ReCluster, group=ReCluster)) +
+  ggplot(aes(x = sample_id, y = freq, fill = ReCluster, group=ReCluster)) +
+  #scale_fill_manual(values=col) + #***
   scale_fill_manual(values=cols) + #**
-  
+  #scale_fill_manual(values=cols_Lineage) + #***
   scale_x_discrete(limits=ordered_names) + #labels= labels
-
   theme(axis.text.y=element_text(size=16), 
         axis.text.x=element_text(size=16, angle = 90),
         axis.title.x = element_text(face="bold", size=18),
         axis.title.y = element_text(face="bold", size=18),
         legend.position = "none") + #    ylab('% PBMC') + xlab('Age groups')
   
-  ylab('% of lineage') + xlab('Individuals (n=95)')
+  ylab('% of PBMCs') + xlab('Individuals (n=167)')
 
 # gg stream 
 BP_Tregs <- BP +   ggstream::geom_stream(color = 'black', 
