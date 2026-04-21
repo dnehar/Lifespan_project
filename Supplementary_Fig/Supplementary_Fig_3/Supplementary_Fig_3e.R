8dd38e48e7d01df9e01a26f740cc3442e3c4b032
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2026-04-21 12:59:59 -0400

    Update Supplementary_Fig_3e.R

--- Supplementary_Fig/Supplementary_Fig_3/Supplementary_Fig_3e.R
+++ Supplementary_Fig/Supplementary_Fig_3/Supplementary_Fig_3e.R
@@ -31,7 +31,7 @@ subset_to_be_plotted <- c('CD14_mono','ISGhi_CD14_mono', 'CD16_mono')
   
 p_corr_pbmc_L4 <- LifeSpan_ALL_MetaData %>%
 
-  mutate(ReCluster = factor(LS_L4)) %>% #***
+  mutate(ReCluster = factor(LS_L4, levels= subset_to_be_plotted)) %>% #***
   mutate(Groups = factor(Age_groups, levels = age_groups)) %>%
   group_by(Groups, sample_id, Age_in_yrs, ReCluster) %>%
   summarise(n = n()) %>% #, Age_months = first(Age_months), Gender = first(Gender)) %>% #, Set = first(Set)
